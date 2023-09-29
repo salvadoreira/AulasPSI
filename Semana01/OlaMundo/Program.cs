a4ccb7044d039202f0e07f23cb38dfbe180effe7
Salvador Eira <salvadoreira15@gmail.com>
2023-09-29 11:16:21 +0100

    Adicionar comentários ao programa OlaMundo

--- Semana01/OlaMundo/Program.cs
+++ Semana01/OlaMundo/Program.cs
@@ -2,10 +2,18 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// Este programa quando iniciado irá mostrar a frase "Hello World!" na consola.
+    /// </summary>
     class Program
     {
+       /// <summary>
+       /// Este método é responsável por imprimir a mensagem.
+       /// </summary>
+       /// <param name="args"> Recebe argumentos da linha de comandos. </param>
         static void Main(string[] args)
         {
+            // Imprime a mensagem
             Console.WriteLine("Hello World!");
         }
     }
