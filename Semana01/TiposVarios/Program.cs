6dfa0a23e6b97672e999a76323db5a8ec99693e8
Salvador Eira <salvadoreira15@gmail.com>
2023-09-29 11:40:16 +0100

    guine

---
+++ Semana01/TiposVarios/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 23;
+            Console.WriteLine(numero);
+            uint i = 4U;
+            console.WriteLine(i);
+            ulong d = 7UL;
+            console.WriteLine(d);
+            long c = 2UL;
+            console.WriteLine(c);
+        }
+    }
+}
