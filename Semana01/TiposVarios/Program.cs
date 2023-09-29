3de43523c1d5b545e67a3c5fa2e6dc109570eb4c
Salvador Eira <salvadoreira15@gmail.com>
2023-09-29 12:16:34 +0100

    Adicionar variáveis char ao programa

--- Semana01/TiposVarios/Program.cs
+++ Semana01/TiposVarios/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace TiposVarios
 {
@@ -6,17 +7,24 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
             //Variáveis Inteiras.
             int numero = 23;
             uint i = 4U;
             ulong d = 7UL;
-            long c = 2UL;
+            long c = 2L;
 
             //Variáveis Reais.
             float n1 = 7f;
             double n2 = 22;
             decimal n3 = 44m;
 
+            //Variáveis Char.
+            char jett = '\u00DF';
+
+            //Variáveis Bool.
+            bool condition = 5 < 3;
 
             Console.WriteLine(numero);
             Console.WriteLine(i);
@@ -25,6 +33,9 @@ namespace TiposVarios
             Console.WriteLine(n1);
             Console.WriteLine(n2);
             Console.WriteLine(n3);
+            Console.WriteLine(jett);
+
+            if(!condition) Console.WriteLine(condition);
         }
     }
 }
