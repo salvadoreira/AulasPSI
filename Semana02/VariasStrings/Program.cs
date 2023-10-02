100d2e5ca856a77edbed9f9ced4071d782f599e2
Salvador Eira <salvadoreira15@gmail.com>
2023-10-02 11:20:06 +0100

    Adicionar variaveis string

---
+++ Semana02/VariasStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            
+            //Variáveis String.
+            string s = "\t \u2D39";
+            Console.WriteLine(s);
+
+            string b = @"C:\Users\Salvador\Documents";
+            Console.WriteLine(b);
+        }
+    }
+}
