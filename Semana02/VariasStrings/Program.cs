2c892efed2630c7bbcec463f74b92cb88a3272a5
Salvador Eira <salvadoreira15@gmail.com>
2023-10-02 12:27:36 +0100

    Adicionar strings interpoladas e concatenadas

--- Semana02/VariasStrings/Program.cs
+++ Semana02/VariasStrings/Program.cs
@@ -9,7 +9,10 @@ namespace VariasStrings
         {
             Console.OutputEncoding = Encoding.UTF8;
             
-            //Variáveis String.
+            // Variáveis de Apoio.
+            int x = 10, y  = 5, r = x + y;
+
+            // Strings Simples e Literais.
             string s = "\t \u2D39";
             Console.WriteLine(s);
 
@@ -18,6 +21,20 @@ namespace VariasStrings
 
             string f = "A Guiné não tem símbolo de copyright \u00A9";
             Console.WriteLine(f);
+
+            // Strings Concatenadas.
+            string bs = "Angola" + " melhor país" + "!";
+            Console.WriteLine(bs);
+            string sf = s + f + 2;
+            Console.WriteLine(sf);
+
+            // Strings Interpoladas.
+            string bf = $"{s} interpolado";
+            Console.WriteLine(bf);
+            string fb = $@"{s} + {b} + {f} verbatim {x}";
+            Console.WriteLine(fb);
+
+            
         }
     }
 }
