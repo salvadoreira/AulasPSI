f21bb12c697d2a313ef70bf0a22a09e36d5d24d4
Salvador Eira <salvadoreira15@gmail.com>
2023-10-02 12:52:17 +0100

    Adicionar ficheiros untracked

---
+++ Argumentos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int 0;
+            for(int i = 0; i<args.Length; i++){
+                Console.WriteLine("");
+
+        }
+    }
+}
