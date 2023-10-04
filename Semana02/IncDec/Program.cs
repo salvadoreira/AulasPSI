b9fa7a6b83686465e1962eaee538c64f6c256094
Salvador Eira <salvadoreira15@gmail.com>
2023-10-04 12:05:53 +0100

    Adicionar variáveis inteiras

---
+++ Semana02/IncDec/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 5;
+            int c = a++ + b--;
+
+            ++a;
+            --b;
+            
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
